1a47627906e3177214169b7d39c9526c1123e1bb
ZenKaban <kabanoff90@gmail.com>
2019-09-04 15:09:15 +0300

    Added counts and ENV variable exception catch

--- 11_lesson/homework/guess_number_fl.py
+++ 11_lesson/homework/guess_number_fl.py
@@ -6,14 +6,20 @@ from wtforms import StringField, validators
 
 app = Flask(__name__)
 
-random.seed(os.environ['FLASK_RANDOM_SEED'])
+try:
+    random.seed(os.environ['FLASK_RANDOM_SEED'])
+except KeyError:
+    print("Set ENV variable FLASK_RANDOM_SEED")
+
 app.config.update(
     DEBUG=True,
     SECRET_KEY='This key must be secret!',
     WTF_CSRF_ENABLED=False,
 )
 
-
+count_success = 0
+count_tries = 0
+guessed = None
 
 class ContactForm(FlaskForm):
     number = StringField(label='Name', validators=[
@@ -22,30 +28,32 @@ class ContactForm(FlaskForm):
 
 def get_random_number():
     guessed = random.randrange(1, 10)
-    with open("number.txt", 'w') as f:
-        f.write(str(guessed))
     return guessed
 
 @app.route('/', methods = ['GET'])
 @app.route('/guess/', methods = ['POST'])
 def home():
+    global count_success
+    global count_tries
+    global guessed
     
     if request.method == "GET":
         guessed = get_random_number()
         print(guessed)
         return "Number generated"
     if request.method == "POST":
+        count_tries += 1
+        print(count_tries)
         number = ContactForm(request.form)
         number_int = int(number.data['number'])
-        with open("number.txt","r") as f:
-            guessed = f.readline()
-            guessed_int = int(guessed)
-        if number_int > guessed_int:
-            result = ">"
-        elif number_int < guessed_int:
-            result = "<"
-        elif number_int == guessed_int:
-            result = "=, new number generated"
+        if number_int > guessed:
+            result = ">, try again, number or tries " + str(count_tries)
+        elif number_int < guessed:
+            result = "<, try again, number or tries " + str(count_tries)
+        elif number_int == guessed:
+            count_success += 1
+            print(count_success)
+            result = "=, success, new number generated, number of successes " + str(count_success)
             guessed = get_random_number()
             print(guessed)
             
